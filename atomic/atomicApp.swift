6cbc5b9c7adf9993402b9cd86d637b07e1438a5b
Vladislav Khorunzhiy <v.m.khorunzhiy@yandex.ru>
2025-09-24 14:54:47 +0300

    Initial Commit

---
+++ atomic/atomicApp.swift
@@ -0,0 +1,17 @@
+//
+//  atomicApp.swift
+//  atomic
+//
+//  Created by Владислав Хорунжий on 24.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct atomicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
